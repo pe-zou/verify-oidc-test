0352b3782e0b6d86f02efab3bc0522c8eae34d0e
pe-zou <pei.tixi.39@gmail.com>
2025-07-28 13:41:35 +0900

    Update oidc-config.js

--- oidc-config.js
+++ oidc-config.js
@@ -1,5 +1,5 @@
 export const oidcConfig = {
-  authority: "https://ciam-ndi.verify.ibm.com",
+  metadataUrl: "https://ciam-ndi.verify.ibm.com/oauth2/.well-known/openid-configuration",
   client_id: "2820b48c-bb13-4244-94d6-424f1390ef88",
   redirect_uri: "https://pe-zou.github.io/verify-oidc-test/callback.html",
   post_logout_redirect_uri: "https://pe-zou.github.io/verify-oidc-test/",
